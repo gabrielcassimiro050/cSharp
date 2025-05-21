72daf51ad05a42d95a3834611b01e2e5c8249f81
gabrielcassimiro050 <163203496+gabrielcassimiro050@users.noreply.github.com>
2025-05-21 16:42:29 -0300

    Add files via upload

---
+++ redes/player_1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+Dictionary<string, char> map = new Dictionary<string, char>();
+const string letters = "abcdefghij";
+
+Main();
+
+void Main()
+{
+    System.Console.WriteLine("1- Posicionamento Aleatório");
+    System.Console.WriteLine("2- Posicionamento Manual");
+    int op = Convert.ToInt32(Console.ReadLine());
+    if (op == 1) posicionamentoAleatorio();
+    else posicionamentoManual();
+
+    displayMap();
+    StartServer(8000);
+}
+
+
+async Task StartServer(int port)
+{
+    var listener = new TcpListener(IPAddress.Any, port);
+    listener.Start();
+    Console.WriteLine($"Aguardando Player2 na porta {port}...");
+    var client = listener.AcceptTcpClient();
+    var stream = client.GetStream();
+    Console.WriteLine("Player2 conectado!");
+}
+
+void displayMap()
+{
+    System.Console.WriteLine("      A   B   C   D   E   F   G   H   I   J  ");
+    System.Console.WriteLine("    -----------------------------------------");
+    for (int x = 0; x < 10; ++x)
+    {
+        if(x+1<10) System.Console.Write((x+1)+"   |");
+        else System.Console.Write((x+1)+"  |");
+        for (int y = 0; y < 10; ++y)
+        {
+            string key = letters[x] + "" + (y + 1);
+            if (map.ContainsKey(key)) System.Console.Write(" " + map[key] + " |");
+            else System.Console.Write("   |");
+        }
+        System.Console.WriteLine(); System.Console.WriteLine("    -----------------------------------------");
+    }
+}
+
+void setMap()
+{
+    for (int x = 0; x < 10; ++x)
+    {
+        for (int y = 0; y < 10; ++y)
+        {
+            string key = letters[x] + "" + (y + 1);
+            map.Add(key, ' ');
+        }
+    }
+}
+
+void posicionamentoAleatorio()
+{
+    Random rand = new Random();
+
+    for (int i = 0; i < 10; ++i)
+    {
+        string key = letters[rand.Next(10)] + "" + rand.Next(10);
+
+        do
+        {
+            key = letters[rand.Next(10)] + "" + rand.Next(10);
+        } while (map.ContainsKey(key));
+
+        map[key] = '*';
+    }
+}   
+
+void posicionamentoManual()
+{
+
+    for (int i = 0; i < 10; ++i)
+    {
+        string coord = Console.ReadLine();
+        map[coord] = '*';
+    }
+
+}
+
---
+++ redes/player_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+Main();
+
+async Task Main()
+{
+    const string host = "127.0.0.1";
+    const int porta = 9000;
+    using var cliente = new TcpClient();
+    await cliente.ConnectAsync(host, porta);
+    using var stream = cliente.GetStream();
+
+    var dados = Encoding.UTF8.GetBytes("Olá servidor!");
+    await stream.WriteAsync(dados, 0, dados.Length);
+
+    var buffer = new byte[1024];
+    int lidos = await stream.ReadAsync(buffer, 0, buffer.Length);
+    Console.WriteLine($"Resposta: {Encoding.UTF8.GetString(buffer, 0, lidos)}");
+}
+
